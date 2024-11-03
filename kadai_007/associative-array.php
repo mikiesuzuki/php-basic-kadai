31813ceacb9899a8b3540822432728355127a7d5
mikiesuzuki <rukia.kaorun@ezweb.ne.jp>
2024-11-03 21:11:16 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang ="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>課題7</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    $vegetable = ['name' => 'onion', 'price'=> 200, 'weight'=> 160];
+
+    // 連想配列の値を出力する
+    print_r($vegetable);
+    ?>
+  </p>
+</body>
+
+</html>
+
